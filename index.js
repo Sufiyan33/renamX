3892a020ac6e9c9cf398ac2eb9f8172db6c6d209
Sufiyan33 <basu700736@gmail.com>
2024-03-13 10:35:22 +0530

    Now successfully renaming file

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const fs = require('fs')
+const path = require('path')
 const replaceThis = "Sufi"
 const replaceWith = "Sufiyan"
+const preview = true
+const folder = __dirname
 
 try{
-    fs.readdir("data", (err, data)=>{
-        console.log(data)
+    fs.readdir(folder, (err, data)=>{
         for (let index = 0; index < data.length; index++) {
             const item = data[index];
-            let newFile = "data/" + item.replaceAll(replaceThis, replaceWith)
-            fs.rename("data/" + item, newFile, ()=>{
-                console.log("Rename Succesful...")
-            })
+            let oldFile = path.join(folder, item)
+            let newFile = path.join(folder, item.replaceAll(replaceThis, replaceWith))
+            if(preview){
+                fs.rename(oldFile, newFile, ()=>{
+                    console.log("Rename Succesful...")
+                })
+            }else{
+                if("data/" + item !== newFile){
+                    console.log("data/" + item + " will be renamed to " + newFile)
+                }
+            }
         }
     })
 }catch(err){
